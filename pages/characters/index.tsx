5d396db50383e3de9dbd994a1e7550941a54f2f9
telonasport99 <telonasport99@gmail.com>
2024-03-15 00:23:02 +0300

    loading at import

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -3,12 +3,14 @@ import {CharacterType, ResponseType} from "assets/api/rick-and-morty-api";
 import {PageWrapper} from "components/PageWrapper/PageWrapper";
 import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
 import dynamic from "next/dynamic";
-import * as module from "module";
 
 const CharacterCard  = dynamic(
     ()=>import('components/Card/CharacterCard/CharacterCard').then(
-    (module)=>module.CharacterCard
-))
+    (module)=>module.CharacterCard),
+    {ssr:false,
+            loading:()=><h1>Loading</h1>
+    }
+    )
 export const getStaticProps = async ()=>{
     const characters = await API.rickAndMorty.getCharacters()
     return {
