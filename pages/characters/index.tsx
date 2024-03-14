8382b0cf9bbe440c86d6cd271be5cce04a8d92a5
telonasport99 <telonasport99@gmail.com>
2024-03-15 00:19:53 +0300

    dynamic import

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,11 +1,14 @@
 import {API} from "assets/api/api";
 import {CharacterType, ResponseType} from "assets/api/rick-and-morty-api";
 import {PageWrapper} from "components/PageWrapper/PageWrapper";
-import {CharacterCard} from "components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
-import {GetStaticPaths} from "next";
+import dynamic from "next/dynamic";
+import * as module from "module";
 
-
+const CharacterCard  = dynamic(
+    ()=>import('components/Card/CharacterCard/CharacterCard').then(
+    (module)=>module.CharacterCard
+))
 export const getStaticProps = async ()=>{
     const characters = await API.rickAndMorty.getCharacters()
     return {
