3ef7b849d0ce881e2fc45557f0623e288ee9288f
telonasport99 <telonasport99@gmail.com>
2024-03-18 18:55:00 +0300

    redirect

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -5,13 +5,23 @@ import {Header} from "components/Header/Header";
 import {Card} from "components/Card/Card";
 import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
 import {GetServerSideProps} from "next";
+import {redirect} from "next/navigation";
 
 export const getServerSideProps:GetServerSideProps = async ({res})=>{
     res.setHeader('Cache-Control','public, s-maxage-10, stale-while-revalidate=100' )
     const episodes = await API.rickAndMorty.getEpisodes()
+    const isAuth = false
     if(!episodes){
         return {
             notFound:true
+        }
+    }
+    if(!isAuth){
+        return {
+            redirect:{
+                destination:'/test',
+                permanent:false
+            }
         }
     }
     return {
